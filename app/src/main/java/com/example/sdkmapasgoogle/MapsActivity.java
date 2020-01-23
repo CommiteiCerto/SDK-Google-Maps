1f0a1c9d61ea02b7f0964241818af5b839677acc
CommiteiCerto <sgl-ti2019>
2020-01-22 23:26:56 -0300

    Criaçao de aplicativo para estudar google maps Capturando a possicao do usuario em tempo real

--- app/src/main/java/com/example/sdkmapasgoogle/MapsActivity.java
+++ app/src/main/java/com/example/sdkmapasgoogle/MapsActivity.java
@@ -1,8 +1,19 @@
 package com.example.sdkmapasgoogle;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,32 +27,100 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private String[] permissoes = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Validar Permissoes
+        Permissoes.validarPermissoes(permissoes, this, 1);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // Objeto responsavel por gerenciar a localizacao do usuario
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("Localizaçõa","onLocationChanged"+ location.toString());
+
+                Double latitude = location.getLatitude();
+                Double longitude = location.getLongitude();
+
+                mMap.clear();
+
+                // Adicione um marcador em Blumenau e mova a câmera
+                LatLng localUsuario = new LatLng(latitude,longitude);
+                //-26.9150432,-49.0656267
+
+                mMap.addMarker(new MarkerOptions()
+                                .position(localUsuario)
+                                .title("\n" + "Meu Local ")
+                                // Trocando a cor do icone
+                                .icon(
+                                        BitmapDescriptorFactory.defaultMarker(
+                                                BitmapDescriptorFactory.HUE_MAGENTA)
+                                )
+                        // Colocando uma img como icone
+                        // .icon(BitmapDescriptorFactory.fromResource(R.drawable.spaceship))
+                );
+
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(localUsuario,18));
+
+                /*Geocoding processo de tranformar um endereço
+                ou descricao de um local em lattidute/longitude
+                Reverse Geocoding processo de tranformar latitude/longitude
+                em um endereço
+                */
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    0,
+                    0,
+                    locationListener
+            );
+
+        }
+
         // Metodo p mudar o mapa
-       // mMap.setMapType(googleMap.MAP_TYPE_SATELLITE);
+        // mMap.setMapType(googleMap.MAP_TYPE_SATELLITE);
 
         // Adicionando evento de clic no mapa
 
@@ -64,24 +143,46 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
+    }
 
-        // Adicione um marcador em Blumenau e mova a câmera
-        LatLng blumenau = new LatLng(-26.9150432, -49.0656267);
-        //-26.9150432,-49.0656267
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        mMap.addMarker(new MarkerOptions()
-                .position(blumenau)
-                .title("\n" + "Marcador em Blumenau")
-               // Trocando a cor do icone
-               /* .icon(
-                        BitmapDescriptorFactory.defaultMarker(
-                                BitmapDescriptorFactory.HUE_MAGENTA)
-                )*/
-               // Colocando uma img como icone
-               .icon(BitmapDescriptorFactory.fromResource(R.drawable.spaceship))
-        );
+        for (int permissaoResultado : grantResults) {
+            if (permissaoResultado == PackageManager.PERMISSION_DENIED) {
+                //Alerta
+                alertaValidacaoPermissao();
 
+            } else if (permissaoResultado == PackageManager.PERMISSION_GRANTED) {
+                //Recuperar localizacao do usuario
+                if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            10000,
+                            1,
+                            locationListener
+                    );
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(blumenau));
+                }
+
+            }
+        }
+    }
+
+    private void alertaValidacaoPermissao(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Permissões Negadas");
+        builder.setMessage("Para utilizar o app é necessário aceitar as permissões: ");
+        builder.setCancelable(false);
+        builder.setPositiveButton("Confirmar ", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
